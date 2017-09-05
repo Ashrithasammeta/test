748d8f19aeedc834c2380a4a99f1b77f3955fa54
sbojja <a-sbojja@ean.com>
2017-09-05 12:39:43 -0500

    test commit

---
+++ src/main/java/com/elsevier/education/Exercise4.java
@@ -0,0 +1,27 @@
+package com.elsevier.education;
+
+/**
+
+TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
+
+*/
+public class Exercise4 {
+
+	public static class Counter {
+		
+		private int count = 0;
+		
+		public int increment() {
+			return ++count;
+		}
+		
+		public synchronized int getCount() {
+			return count;
+		}
+		
+		public synchronized void resetCount() {
+			count = 0;
+		}
+
+	}
+}
